c0ac0f9a08394d9ba603b2de40779b216180fd8e
Larry Chan <larrylaren2@gmail.com>
2025-04-16 18:16:04 +0800

    Instructions completed till 2nnn.

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,4 +1,5 @@
 #include "chip8.hpp"
+
 #include <fstream>
 
 const unsigned int START_ADDRESS = 0x200;
@@ -6,34 +7,33 @@ const unsigned int START_ADDRESS = 0x200;
 const unsigned int FONTSET_SIZE = 80;
 const unsigned int FONTSET_START_ADDRESS = 0x50;
 
-uint8_t fontset[FONTSET_SIZE] =
-	{
-		0xF0, 0x90, 0x90, 0x90, 0xF0,// 0
-		0x20, 0x60, 0x20, 0x20, 0x70,// 1
-		0xF0, 0x10, 0xF0, 0x80, 0xF0,// 2
-		0xF0, 0x10, 0xF0, 0x10, 0xF0,// 3
-		0x90, 0x90, 0xF0, 0x10, 0x10,// 4
-		0xF0, 0x80, 0xF0, 0x10, 0xF0,// 5
-		0xF0, 0x80, 0xF0, 0x90, 0xF0,// 6
-		0xF0, 0x10, 0x20, 0x40, 0x40,// 7
-		0xF0, 0x90, 0xF0, 0x90, 0xF0,// 8
-		0xF0, 0x90, 0xF0, 0x10, 0xF0,// 9
-		0xF0, 0x90, 0xF0, 0x90, 0x90,// A
-		0xE0, 0x90, 0xE0, 0x90, 0xE0,// B
-		0xF0, 0x80, 0x80, 0x80, 0xF0,// C
-		0xE0, 0x90, 0x90, 0x90, 0xE0,// D
-		0xF0, 0x80, 0xF0, 0x80, 0xF0,// E
-		0xF0, 0x80, 0xF0, 0x80, 0x80 // F
+uint8_t fontset[FONTSET_SIZE] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0,  // 0
+    0x20, 0x60, 0x20, 0x20, 0x70,  // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0,  // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0,  // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10,  // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0,  // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0,  // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40,  // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0,  // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0,  // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90,  // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0,  // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0,  // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0,  // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0,  // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80   // F
 };
 
 Chip8::Chip8()
-	: randGen(std::chrono::system_clock::now().time_since_epoch().count()) {
+    : randGen(std::chrono::system_clock::now().time_since_epoch().count()) {
   // Initialize Program Counter (PC)
   pc = START_ADDRESS;
 
   // Load fonts into memory
   for (unsigned int i = 0; i < FONTSET_SIZE; i++) {
-	memory[FONTSET_START_ADDRESS + 1] = fontset[i];
+    memory[FONTSET_START_ADDRESS + 1] = fontset[i];
   }
 
   // Initialize RNG
@@ -41,31 +41,148 @@ Chip8::Chip8()
 }
 
 void Chip8::LoadROM(const char *filename) {
-
-  // Open file as a stream of binary and move file pointer to the end(ate = at end).
+  // Open file as a stream of binary and move file pointer to the end(ate = at
+  // end).
   std::ifstream file(filename, std::ios::binary | std::ios::ate);
 
   if (file.is_open()) {
+    // Get size of file and allocate a buffer to hold content
+    std::streampos size = file.tellg();
+    char *buffer = new char[size];
 
-	// Get size of file and allocate a buffer to hold content
-	std::streampos size = file.tellg();
-	char *buffer = new char[size];
+    // Go back to the beginning of the file.
+    file.seekg(0, std::ios::beg);
 
-	// Go back to the beginning of the file.
-	file.seekg(0, std::ios::beg);
+    // read() reads the ROM into memory/buffer.
+    // Fill "size" bytes into the "buffer"(destination).
+    file.read(buffer, size);
 
-	// read() reads the ROM into memory/buffer.
-	// Fill "size" bytes into the "buffer"(destination).
-	file.read(buffer, size);
+    // Already loaded the ROM into buffer, close the file.
+    file.close();
 
-	// Already loaded the ROM into buffer, close the file.
-	file.close();
+    // Load ROM contents into CHIP-8's memory, starting from 0x200.
+    for (long i = 0; i < size; i++) {
+      memory[START_ADDRESS + i] = buffer[i];
+    }
 
-	// Load ROM contents into CHIP-8's memory, starting from 0x200.
-	for (long i = 0; i < size; i++) {
-	  memory[START_ADDRESS + i] = buffer[i];
-	}
-
-	delete[] buffer;
+    delete[] buffer;
   }
 }
+
+void Chip8::OP_NULL() {}
+
+// CLS
+void Chip8::OP_00E0() { memset(video, 0, sizeof(video)); }
+
+// RET
+void Chip8::OP_00EE() {
+  // Requires safety check for stack level below 0,
+  // unsigned int wraparound if out of 0 to 255.
+  if (sp == 0) {
+    pc = stack[sp];  // Needs error handling.
+  } else {
+    // Move sp down.
+    sp--;
+    pc = stack[sp];
+  }
+}
+
+// Jump to address nnn
+void Chip8::OP_1nnn() {
+  // extracting last 12-bit (nnn) of the opcode with bitwise AND,
+  // example:
+  // opcode:  0001 0010 1010 0000  (0x12A0)
+  // 0x0FFF:  0000 1111 1111 1111  (0x0FFF)
+  //          -------------------
+  // Result:  0000 0010 1010 0000  (0x02A0)
+
+  uint16_t addr = opcode & 0x0FFFu;
+  pc = addr;
+}
+
+// Call subroutine at address nnn
+// Analogy: You were told to visit a place (nnn),
+// but you plan to return where you are now.
+void Chip8::OP_2nnn() {
+  // Put current PC onto the top of the stack.
+  // Analogy: Before leaving your current place,
+  // you write down the next place's address in your notebook(stack)
+  stack[sp] = pc;
+
+  // Move SP up one level.
+  // Safety check to prevent overflow & wrap around.
+  // Analogy: You start a new page on your notebook (sp++),
+  if (sp >= STACK_LEVELS) {
+    sp = STACK_LEVELS;  // Needs error handling.
+  } else {
+    sp++;
+  }
+
+  // PC jumps to the address nnn
+  // Analogy: then you move to the new place (nnn).
+  uint16_t addr = opcode & 0x0FFFu;
+  pc = addr;
+
+  // Analogy: The notebook has record of your last address,
+  // so you can return to it with RET(00EE).
+}
+
+void Chip8::OP_3xkk() {}
+
+void Chip8::OP_4xkk() {}
+
+void Chip8::OP_5xy0() {}
+
+void Chip8::OP_6xkk() {}
+
+void Chip8::OP_7xkk() {}
+
+void Chip8::OP_8xy0() {}
+
+void Chip8::OP_8xy1() {}
+
+void Chip8::OP_8xy2() {}
+
+void Chip8::OP_8xy3() {}
+
+void Chip8::OP_8xy4() {}
+
+void Chip8::OP_8xy5() {}
+
+void Chip8::OP_8xy6() {}
+
+void Chip8::OP_8xy7() {}
+
+void Chip8::OP_8xyE() {}
+
+void Chip8::OP_9xy0() {}
+
+void Chip8::OP_Annn() {}
+
+void Chip8::OP_Bnnn() {}
+
+void Chip8::OP_Cxkk() {}
+
+void Chip8::OP_Dxyn() {}
+
+void Chip8::OP_Ex9E() {}
+
+void Chip8::OP_ExA1() {}
+
+void Chip8::OP_Fx07() {}
+
+void Chip8::OP_Fx0A() {}
+
+void Chip8::OP_Fx15() {}
+
+void Chip8::OP_Fx18() {}
+
+void Chip8::OP_Fx1E() {}
+
+void Chip8::OP_Fx29() {}
+
+void Chip8::OP_Fx33() {}
+
+void Chip8::OP_Fx55() {}
+
+void Chip8::OP_Fx65() {}
--- src/chip8.hpp
+++ src/chip8.hpp
@@ -23,6 +23,8 @@ class Chip8 {
   // ========== Instruction Set ==========
   // =====================================
   // There are total of 34 instructions for CHIP-8
+  // (35 if you include the first one that is useless.)
+  void OP_NULL();
 
   // 00E0: CLS (Clear the display).
   // Set the entire video buffer to zeroes.
@@ -37,6 +39,136 @@ class Chip8 {
   // 1nnn: JP addr (Jump to location nnn).
   // Sets the PC to nnn.
   void OP_1nnn();
+
+  // 2nnn: CALL addr (Starts a subroutine at address nnn).
+  // Save the current PC in your stack, then set PC to nnn.
+  void OP_2nnn();
+
+  // 3xkk: SE Vx, byte.
+  // Skips the next instruction if register Vx equals kk(a number).
+  void OP_3xkk();
+
+  // 4xkk: SNE Vx, byte.
+  // Skips the next instruction if Vx does not equal to kk.
+  void OP_4xkk();
+
+  // 5xy0: SE Vx, Vy.
+  // Skips the next instruction if Vx equals Vy.
+  void OP_5xy0();
+
+  // 6xkk: LD Vx, byte.
+  // Puts the number kk into register Vx.
+  void OP_6xkk();
+
+  // 7xkk: ADD Vx, byte.
+  // Adds kk to Vx (no extra flag stuff)
+  void OP_7xkk();
+
+  // 8xy0: LD Vx, Vy.
+  // Copies Vy into Vx.
+  void OP_8xy0();
+
+  // 8xy1: OR Vx, Vy.
+  // Bitwise OR between Vx and Vy, puts the result in Vx.
+  void OP_8xy1();
+
+  // 8xy2: AND Vx, Vy.
+  // Bitwise AND, stores it in Vx.
+  void OP_8xy2();
+
+  // 8xy3: XOR Vx, Vy.
+  // Bitwise XOR, stores it in Vx.
+  void OP_8xy3();
+
+  // 8xy4: AND Vx, Vy.
+  // Adds Vy to Vx, sets VF to 1 if it overflows(goes over 255)
+  void OP_8xy4();
+
+  // 8xy5: SUB Vx, Vy.
+  // Subtracts Vy from Vx, sets VF to 1 if no borrow (Vx >= Vy).
+  void OP_8xy5();
+
+  // 8xy6: SHR Vx.
+  // If the least-significant bit of Vx is 1,
+  // then VF is set to 1, otherwise 0.
+  // Then Vx is divided by 2.
+  void OP_8xy6();
+
+  // 8xy7: SUBN Vx, Vy.
+  // Set Vx = Vy - Vx, set VF = NOT borrow.
+  // If Vy > Vx, then VF is set to 1, otherwise 0.
+  // Then Vx is subtracted from Vy, and the results stored in Vx.
+  void OP_8xy7();
+
+  // 8xyE: SHL Vx {, Vy}.
+  // Vx left shift 1, most significant bit is saved in register VF.
+  void OP_8xyE();
+
+  // 9xy0: SNE Vx, Vy.
+  // Skips to the next instruction if Vx != Vy.
+  void OP_9xy0();
+
+  // Annn: LD I, addr.
+  // Sets I(Index) to address nnn.
+  void OP_Annn();
+
+  // Bnnn: JP V0, addr.
+  // Jumps to nnn + V0.
+  void OP_Bnnn();
+
+  // Cxkk: RND Vx, byte.
+  // Puts a random number (0-255) bitwise ANDed with kk, stores into Vx.
+  void OP_Cxkk();
+
+  // Dxyn: DRW Vx, Vy, nibble.
+  // Draws an N-byte sprite from memory (at I(Index)) onto the screen at (Vx, Vy).
+  // VF = 1 if pixels collide.
+  void OP_Dxyn();
+
+  // Ex9E: SKP Vx.
+  // Skips next instruction if the key with the value Vx is pressed.
+  void OP_Ex9E();
+
+  // ExA1: SKNP Vx.
+  // Skips next instruction if the key with the value Vx is not pressed.
+  void OP_ExA1();
+
+  // Fx07: LD Vx, DT.
+  // Puts the delay timer value into Vx.
+  void OP_Fx07();
+
+  // Fx0A: LD Vx, K.
+  // Waits for a key press, then puts the key's value into Vx.
+  void OP_Fx0A();
+
+  // Fx15: LD DT, Vx.
+  // Sets the delay timer to Vx.
+  void OP_Fx15();
+
+  // Fx18: LD ST, Vx.
+  // Sets the sound timer to Vx.
+  void OP_Fx18();
+
+  // Fx1E: ADD I, Vx.
+  // Adds Vx to I(Index).
+  void OP_Fx1E();
+
+  // Fx29: LD F, Vx.
+  // Sets I(Index) to the address of a sprite (0-9) for Vx.
+  void OP_Fx29();
+
+  // Fx33: LD B, Vx.
+  // Stores Vx as 3 digits (hundreds, tens, units) at I, I+1, I+2.
+  void OP_Fx33();
+
+  // Fx55: LD[I], Vx.
+  // Copies V0 to Vx into memory starting at I.
+  void OP_Fx55();
+
+  // Fx65: LD Vx, [I].
+  // Loads V0 to Vx from memory starting at I.
+  void OP_Fx65();
+
 
   // 16x 8-bit registers, from V0 to VF, holds 0x00 to 0xFF.
   uint8_t registers[REGISTER_COUNT]{};
